c0f733e2c1db699fb32b86ae4a9a3db6479253e1
aunwat <za0971872197@gmail.com>
2025-06-18 10:41:22 +0700

    v.3

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <> 
-      <h1> Mr Aunwat somte </h1>
+      <h1><caches></caches> Mr Aunwat somte </h1>
     </>
 
   );
